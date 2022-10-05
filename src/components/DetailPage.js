2cec1bb3dae8d080f694e207d6f2c2e5d1762b88
Nabeela <nabeela.samsi.in@gmail.com>
2022-10-05 17:46:21 +0530

    Prettier

--- src/components/DetailPage.js
+++ src/components/DetailPage.js
@@ -1,49 +1,52 @@
 import { useEffect, useState } from "react";
-import { useNavigate, useParams } from "react-router-dom"
+import { useNavigate, useParams } from "react-router-dom";
 
 const DetailPage = (props) => {
-    const { id } = useParams();
-    const [userData, setUserData] = useState({});
-    const [loading, setLoading] = useState(true);
-    const navigate = useNavigate();
+  const { id } = useParams();
+  const [userData, setUserData] = useState({});
+  const [loading, setLoading] = useState(true);
+  const navigate = useNavigate();
 
-    useEffect(() => {
-        fetch(`https://jsonplaceholder.typicode.com/users/${id}`)
-        .then(setLoading(true))
-        .then(async (data) => {
-            let responseData = await data.json();
-            if (responseData) {
-                setUserData(responseData);
-             }
-             setLoading(false);
-        })
-    }, [id]);
+  useEffect(() => {
+    fetch(`https://jsonplaceholder.typicode.com/users/${id}`)
+      .then(setLoading(true))
+      .then(async (data) => {
+        let responseData = await data.json();
+        if (responseData) {
+          setUserData(responseData);
+        }
+        setLoading(false);
+      });
+  }, [id]);
 
-    return (
-        loading ? <p>Loading...</p> :
-        (<div className="detailPage">
-            <div >
-                <button onClick={() => navigate(-1)}>Back</button>
-                <p>name: {userData.name}</p>
-                <p>username: {userData.username}</p>
-                <p>email: {userData.email}</p>
-                <p>phone: {userData.phone}</p>
-                <p>company: {userData.company.name}</p>
-                <p>website: {userData.website}</p>
-                <p>address</p>
-                <ul>
-                    {Object.entries(userData.address).map((data, index) => {
-                        if(data[0] !== 'geo') {
-                            return (<li key={`${data[0]}~${index}`}>{`${data[0]}: ${data[1]}`}</li>)
-                        } else {
-                            return null;
-                        }
-                    })}
-                </ul>
-            </div>
-            <br />
-        </div>)
-    )
-}
+  return loading ? (
+    <p>Loading...</p>
+  ) : (
+    <div className="detailPage">
+      <div>
+        <button onClick={() => navigate(-1)}>Back</button>
+        <p>name: {userData.name}</p>
+        <p>username: {userData.username}</p>
+        <p>email: {userData.email}</p>
+        <p>phone: {userData.phone}</p>
+        <p>company: {userData.company.name}</p>
+        <p>website: {userData.website}</p>
+        <p>address</p>
+        <ul>
+          {Object.entries(userData.address).map((data, index) => {
+            if (data[0] !== "geo") {
+              return (
+                <li key={`${data[0]}~${index}`}>{`${data[0]}: ${data[1]}`}</li>
+              );
+            } else {
+              return null;
+            }
+          })}
+        </ul>
+      </div>
+      <br />
+    </div>
+  );
+};
 
-export {DetailPage as default}
+export { DetailPage as default };
--- src/components/Home.js
+++ src/components/Home.js
@@ -2,23 +2,24 @@ import { useEffect, useState } from "react";
 import UserCard from "./UserCard";
 
 const Home = () => {
-    const [userData, setUserData] = useState([]);
+  const [userData, setUserData] = useState([]);
 
-    useEffect(() => {
-        fetch('https://jsonplaceholder.typicode.com/users')
-        .then(async (data) => {
-            let responseData = await data.json();
-            setUserData(responseData);
-        });
-    },[]);
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users").then(async (data) => {
+      let responseData = await data.json();
+      setUserData(responseData);
+    });
+  }, []);
 
-    return(
-        <div>
-            <div className="grid-container">
-                {userData.map((data) => <UserCard key={data.id} data={data} />)}
-            </div>
-        </div>
-    )
-}
+  return (
+    <div>
+      <div className="grid-container">
+        {userData.map((data) => (
+          <UserCard key={data.id} data={data} />
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default Home;
